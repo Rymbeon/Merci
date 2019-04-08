2505db29d66fbab607c02829eda7a05d0baf5fc0
Gleb Danilenko <glebdanilenko@gmail.com>
2019-04-08 13:40:37 +0300

    slide

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
+ 
+$(document).ready(function(){
+	$("#nav-list").on("click","a", function (event) {
+
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+
+			top = $(id).offset().top;
+		
+    $('body,html').animate({scrollTop: top}, 1500);
+    
+    $("#nav-list").css('top', '-500px');
+  });
+  $("#burger").click( function () {
+    $("#nav-list").css('top', '0');
+	});
+});
